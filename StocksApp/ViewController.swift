0033de92550b72a254a43a8dd71f8544a92df31d
Дмитрий Кара <kara@group-ib.com>
2020-12-01 18:56:29 +0300

    Solve additional tasks 3 & 5 (and 4 +-)

--- StocksApp/ViewController.swift
+++ StocksApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     @IBOutlet weak var Image: UIImageView!
@@ -16,26 +17,113 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var companyPicker: UIPickerView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    private let companies: [String:String] = [
-        "Apple":"AAPL",
-        "Microsoft":"MSFT",
-        "Google":"GOOG",
-        "Amazon":"AMZN",
-        "Facebook":"FB",
-    ]
+    private var companies: [String:String] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.companyPicker.dataSource = self
-        self.companyPicker.delegate = self
-        
         self.activityIndicator.hidesWhenStopped = true
         
-        self.requestQuoteUpdate()
+        self.downloadCompanies()
     }
     
     // MARK: - Private functions
+    
+    private func showResponseAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+
+        alert.addAction(
+            UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel))
+    
+        alert.addAction(
+            UIAlertAction(title: "Exit", style: UIAlertAction.Style.default, handler: {(_: UIAlertAction!) in
+                exit(0)
+            }))
+        
+        DispatchQueue.main.async() {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func showExitAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+    
+        alert.addAction(
+            UIAlertAction(title: "Exit", style: UIAlertAction.Style.default, handler: {(_: UIAlertAction!) in
+                exit(0)
+            }))
+        
+        DispatchQueue.main.async() {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func downloadCompanies() {
+        let url = URL(string: "https://sandbox.iexapis.com/stable/stock/market/list/mostactive?listLimit=20&token=Tsk_b569ab301292462c96947f38cb2d814e")!
+        
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForResource = 2
+        let session = URLSession(configuration: config)
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else {
+                print("! Network error")
+                self.showExitAlert(title: "Pure internet connection", message: "Please check your internet connection")
+                return
+            }
+            self.parseCompanies(data: data)
+        }
+        
+        dataTask.resume()
+    }
+    
+    private func parseCompanies(data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data)
+            
+            guard
+                let array = jsonObject as? [[String:Any]]
+            else {
+                print("! Invalid JSON format")
+                self.showExitAlert(title: "Bad server response", message: "Please try again")
+                return
+            }
+            
+            for comp in array {
+                guard
+                    let companyName = comp["companyName"] as? String,
+                    let symbol = comp["symbol"] as? String
+                else {
+                    print("! Invalid JSON format")
+                    self.showExitAlert(title: "Bad server response", message: "Please try again")
+                    return
+                }
+                self.companies[companyName] = symbol
+            }
+            
+            DispatchQueue.main.async() {
+                self.companyPicker.dataSource = self
+                self.companyPicker.delegate = self
+                self.requestQuoteUpdate()
+            }
+        }
+        catch {
+            print("! JSON parsing error: " + error.localizedDescription)
+            self.showExitAlert(title: "Bad server response", message: "Please restart app")
+        }
+    }
     
     private func requestQuoteUpdate() {
         self.activityIndicator.startAnimating()
@@ -54,13 +142,18 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     private func requestQuote(for symbol:String) {
         let url = URL(string: "https://sandbox.iexapis.com/stable/stock/\(symbol.lowercased())/batch?types=quote&token=Tsk_b569ab301292462c96947f38cb2d814e")!
         
-        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForResource = 2
+        let session = URLSession(configuration: config)
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
             guard
                 error == nil,
                 (response as? HTTPURLResponse)?.statusCode == 200,
                 let data = data
             else {
                 print("! Network error")
+                self.showResponseAlert(title: "Pure internet connection", message: "Please check your internet connection")
                 return
             }
             self.parseQuote(data: data)
@@ -72,12 +165,17 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     private func downloadImage(for symbol: String) {
         let url = URL(string: "https://storage.googleapis.com/iex/api/logos/\(symbol).png")!
         
-        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForResource = 2
+        let session = URLSession(configuration: config)
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
             guard
                 let data = data,
                 error == nil
             else {
                 print("! Can't load image")
+                self.showResponseAlert(title: "Pure internet connection", message: "Please check your internet connection")
                 return
             }
             DispatchQueue.main.async() {
@@ -100,6 +198,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                 let change = quote["change"] as? Double
             else {
                 print("! Invalid JSON format")
+                self.showResponseAlert(title: "Bad server response", message: "Please try again")
                 return
             }
             DispatchQueue.main.async {
@@ -108,6 +207,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
         catch {
             print("! JSON parsing error: " + error.localizedDescription)
+            self.showResponseAlert(title: "Bad server response", message: "Please try again")
         }
     }
     
